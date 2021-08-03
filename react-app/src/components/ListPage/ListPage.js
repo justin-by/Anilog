46731bb1472e61a51cb1ead25770fc3223c523f4
justin-by <flaminlinkvp@gmail.com>
2021-08-03 14:14:57 -0700

    anime list title/score div

--- react-app/src/components/ListPage/ListPage.js
+++ react-app/src/components/ListPage/ListPage.js
@@ -4,9 +4,40 @@ const ListPage = () => {
 
     return (
         <>
-            <div className='background'>
+            <div className='background2'>
                 <div className='container'>
+                    <div className='list-side-info'>
+                    </div>
+                    <div className='lists'>
+                        <div className='list-header'>
+                            <h3>Completed</h3>
+                        </div>
+                        <div className='list-section'>
+                            <div className='list-head-row'>
+                                <div className='list-title'>
+                                Title 
+                                </div>
+                                <div className='list-score'>
+                                Score
+                                </div>
+                            </div>
+                            <div className='list-entry-row'>
+                                <div className='list-entry-cover'>
+                                    <div className='list-entry-img'>
 
+                                    </div>
+                                </div>
+                                <div className='list-entry-title'>
+                                    <a>
+                                        Shingeki no Kyojin
+                                    </a>
+                                </div>
+                                <div className='list-entry-score'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
