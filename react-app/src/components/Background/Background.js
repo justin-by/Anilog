504e0e5d32163788c47482223606abc9d4b22be9
justin-by <flaminlinkvp@gmail.com>
2021-08-26 15:29:35 -0700

    refactoring splash page (removed logo/about links)

--- react-app/src/components/Background/Background.js
+++ react-app/src/components/Background/Background.js
@@ -14,19 +14,8 @@ const HomePage = () => {
                             </div>
                         </div>
 
-                        <div className='home-image-wrapper'>
-                            <img className='home-image' alt='anime eye' src='https://i.imgur.com/ItMIOyV.png' />
-                        </div>
 
                     </div>
-                </div>
-                <div className='about-me-links'>
-                    <a href='https://github.com/justin-by' target="_blank" rel="noreferrer">
-                        <img src="https://img.icons8.com/ios/100/000000/github--v2.png" alt='github icon' />
-                    </a>
-                    <a href='https://www.linkedin.com/in/justin-sung-1877921b4/' target="_blank" rel="noreferrer">
-                        <img src="https://img.icons8.com/ios/100/000000/linkedin-2--v2.png" alt='linkedin icon' />
-                    </a>
                 </div>
             </div>
         </>
