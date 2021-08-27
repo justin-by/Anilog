25df666c9a1d6b4d33e490899ad927dd8dfc0f94
justin-by <flaminlinkvp@gmail.com>
2021-08-27 12:56:20 -0700

    finished refactoring splash page, added github, linkedin, and angellist icons

--- react-app/src/components/Background/Background.js
+++ react-app/src/components/Background/Background.js
@@ -12,6 +12,20 @@ const HomePage = () => {
                             <div className='welcome-under-wrapper'>
                                 <span className='welcome-under-text'>Discover and organize anime in your own personalized list</span>
                             </div>
+                            <div className='home-logo-container'>
+                                <img src='https://i.imgur.com/W6HzdZ5.png' className='home-logo'></img>
+                            </div>
+                            <div className='about-links-container'>
+                                <a href='https://github.com/justin-by' target="_blank" rel="noopener noreferrer">
+                                    <i class="fab fa-github about-link"></i>
+                                </a>
+                                <a href='https://www.linkedin.com/in/j-sung/' target="_blank" rel="noopener noreferrer">
+                                    <i class="fab fa-linkedin-in about-link"></i>
+                                </a>
+                                <a href='https://angel.co/u/justin-sung-6' target="_blank" rel="noopener noreferrer">
+                                    <i class="fab fa-angellist about-link"></i>
+                                </a>
+                            </div>
                         </div>
 
 
