16489505d4ff9fdc8a665cc3aca1975c5df6a3a6
Justin Sung <jsung.by@gmail.com>
2024-09-23 14:12:18 -0700

    Testing procfile

--- app/config.py
+++ app/config.py
@@ -8,6 +8,5 @@ class Config:
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = "postgres://ubo4jts6asn503:pbd17cafcdf9f25e1c3c4af6cc2cd6a47907bc0b6a6d7423737069c8674af6b44@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dbmf1qmbkhekdp"
     SQLALCHEMY_ECHO = True
